f3f797b4620124ae7f569bd67ee9c9f5225fd93b
Michael Rose <michael_rose@outlook.com>
2017-11-04 18:04:51 -0400

    Refactor for readability

--- index.js
+++ index.js
@@ -1,77 +1,91 @@
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
+
 const babylon = require('babylon');
 const traverse = require('babel-traverse').default;
 const generate = require('babel-generator').default;
-const util = require('util');
-const fs = require('fs');
-const path = require('path');
-const readFile = util.promisify(fs.readFile);
 const template = require('babel-template');
 const t = require('babel-types');
 
-const entry = require.resolve(process.argv.slice(2)[0]);
+const [entry] = process.argv.slice(2);
 
-const filesToProcess = [entry];
+const entryAbsolutePath = require.resolve(entry);
 
-const modules = {};
+const filesToProcess = [entryAbsolutePath];
+const moduleAsts = {};
 
-(async() => {
-  while(filesToProcess.length > 0) {
-    const file = filesToProcess.pop();
-    const code = await readFile(file, 'utf8');
-    const ast = babylon.parse(code);
-    traverse(ast, {
-      CallExpression({ node }) {
-        if(node.callee.name === 'require') {
-          const dependencyFile = require.resolve(
-            path.join(
-              path.dirname(file),
-              node.arguments[0].value
-            )
-          );
-          node.arguments[0].value = dependencyFile;
-          if(!modules[dependencyFile]) {
-            filesToProcess.push(dependencyFile);
-          }
-        }
+while(filesToProcess.length > 0) {
+  const fileAbsolutePath = filesToProcess.pop();
+  const code = fs.readFileSync(fileAbsolutePath, 'utf8');
+  const ast = babylon.parse(code);
+
+  traverse(ast, {
+    CallExpression({ node }) {
+      if(node.callee.name !== 'require') {
+        return;
       }
-    });
-    modules[file] = ast;
-  }
 
-  const buildModule = template(`
-    modules[MODULE_ID] = function(require, exports, module) {
-      MODULE_CONTENTS
+      const dependencyRelativePath = node.arguments[0].value;
+      const dependencyResolutionPath = path.join(
+        path.dirname(fileAbsolutePath),
+        dependencyRelativePath
+      );
+      const dependencyAbsolutePath = require.resolve(dependencyResolutionPath);
+
+      const moduleId = dependencyAbsolutePath;
+
+      node.arguments[0] = t.stringLiteral(moduleId);
+
+      if(!moduleAsts[moduleId]) {
+        filesToProcess.push(dependencyAbsolutePath);
+      }
+    }
+  });
+
+  const moduleId = fileAbsolutePath;
+
+  moduleAsts[moduleId] = ast;
+}
+
+const buildModuleRegistration = template(`
+  modules[MODULE_ID] = function(require, exports, module) {
+    MODULE_CONTENTS
+  };
+`);
+
+const moduleRegistrationAsts = Object.entries(moduleAsts).map(([id, ast]) => {
+  return buildModuleRegistration({
+    MODULE_ID: t.stringLiteral(id),
+    MODULE_CONTENTS: ast
+  });
+});
+
+const buildBundle = template(`
+  void (function() {
+    var modules = {};
+    MODULE_REGISTRATIONS
+
+    var loadedModules = {};
+    var weepackRequire = function(moduleId) {
+      if(!loadedModules[moduleId]) {
+        var module = { exports: {} };
+        modules[moduleId](weepackRequire, module.exports, module);
+        loadedModules[moduleId] = module.exports;
+      }
+      return loadedModules[moduleId];
     };
-  `);
 
-  const moduleAsts = [];
+    weepackRequire(ENTRY_MODULE_ID);
+  })();
+`);
 
-  for(const moduleId in modules) {
-    const ast = buildModule({
-      MODULE_ID: t.stringLiteral(moduleId),
-      MODULE_CONTENTS: modules[moduleId]
-    });
-    moduleAsts.push(ast);
-  }
+const bundleAst = buildBundle({
+  MODULE_REGISTRATIONS: moduleRegistrationAsts,
+  ENTRY_MODULE_ID: t.stringLiteral(entryAbsolutePath)
+});
 
-  const buildBundle = template(`
-    var bundle = (function() {
-      var modules = {};
-      MODULES
-      var loadedModules = {};
-      var weepackRequire = function(moduleId) {
-        if(!loadedModules[moduleId]) {
-          var module = { exports: {} };
-          modules[moduleId](weepackRequire, module.exports, module);
-          loadedModules[moduleId] = module.exports;
-        }
-        return loadedModules[moduleId];
-      };
-      weepackRequire(ENTRY);
-    })();
-  `);
+const bundleCode = generate(bundleAst).code;
 
-  console.log(generate(buildBundle({ MODULES: moduleAsts, ENTRY: t.stringLiteral(entry) })).code);
-})();
+console.log(bundleCode);
